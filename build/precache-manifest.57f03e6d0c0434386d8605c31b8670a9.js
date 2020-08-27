2907c036aa6ce4ecb1f099089802e57a5bd5f90c
marfru <hola@marfru.com>
2020-08-27 15:22:52 +0200

    build

---
+++ build/precache-manifest.57f03e6d0c0434386d8605c31b8670a9.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be287155d7c18c0ce75c8cc8148bfd81",
+    "url": "/index.html"
+  },
+  {
+    "revision": "65ee7c21fea3433e29d3",
+    "url": "/static/css/main.e1019cbd.chunk.css"
+  },
+  {
+    "revision": "e22a5ed3e1bc85766d5a",
+    "url": "/static/js/2.c45dfd8d.chunk.js"
+  },
+  {
+    "revision": "90565ba0b43e1b95d2876295dfd7014d",
+    "url": "/static/js/2.c45dfd8d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65ee7c21fea3433e29d3",
+    "url": "/static/js/main.402f9a9e.chunk.js"
+  },
+  {
+    "revision": "335e81980f04d7daa31e",
+    "url": "/static/js/runtime-main.831a919c.js"
+  },
+  {
+    "revision": "a1e04a413333039f8590cefadb42bdc1",
+    "url": "/static/media/house.a1e04a41.jpeg"
+  },
+  {
+    "revision": "a162e56c5c27bd235d3297c344759c9a",
+    "url": "/static/media/model3.a162e56c.jpeg"
+  },
+  {
+    "revision": "4e53f69f9f28c75a603577575401f006",
+    "url": "/static/media/models.4e53f69f.jpeg"
+  },
+  {
+    "revision": "99280c76175f0ff68df5b0b4f8a9e3ee",
+    "url": "/static/media/modelx.99280c76.jpeg"
+  },
+  {
+    "revision": "e2b44db24f53e19bfd69dcc61ab1ee66",
+    "url": "/static/media/modely.e2b44db2.jpeg"
+  },
+  {
+    "revision": "fe70c48e08276958fd091533705d1529",
+    "url": "/static/media/solar.fe70c48e.jpeg"
+  }
+]);
